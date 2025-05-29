2374ef3a3a69c18ced2d97bd41708236f35d1710
theashu02 <ashutoshchauhan1919@gmail.com>
2025-05-29 23:08:39 +0530

    cors

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -3,7 +3,13 @@ import http from "http";
 import express from "express";
 
 const app = express();
-
+app.use(
+	cors({
+	  origin: "https://simp-lx.vercel.app", 
+	  credentials: true, 
+	  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+	})
+  );
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
