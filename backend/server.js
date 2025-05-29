743fa0fded44c12696b107544c0a3b555a2e873b
theashu02 <ashutoshchauhan1919@gmail.com>
2025-05-29 22:22:33 +0530

    allow origin

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
 	cors: {
-		origin: [process.env.CLIENT_URL],
+		origin: "*",
 		methods: ["GET", "POST"],
 	},
 });
