c6925ca7743cdba2a5d8b75121d8d69d0c16a331
theashu02 <ashutoshchauhan1919@gmail.com>
2025-05-29 20:34:23 +0530

    add the env and port to frontend deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: import.meta.env.VITE_SOCKET_URL,
+        target: "http://localhost:5000",
         changeOrigin: true,
       }
     }
